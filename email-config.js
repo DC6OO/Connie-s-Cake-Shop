c6e26b5c338e24ef0284c3cce3f1b7384333faf1
DC6OO <104395715+DC6OO@users.noreply.github.com>
2026-07-14 10:43:08 +0200

    Add files via upload

---
+++ email-config.js
@@ -0,0 +1,6 @@
+// Optional: get a free access key from https://web3forms.com
+// (enter Kabusweck311@gmail.com, then paste the key below)
+// If left empty, FormSubmit is used instead (needs one-time Activate Form click).
+window.EMAIL_CONFIG = {
+  web3formsAccessKey: "",
+};
